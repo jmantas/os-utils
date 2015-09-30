cc721e5de39e5924147642c2fbdaa6941037dad9
jmantas <jmantas@users.noreply.github.com>
2015-09-30 18:04:04 +0300

    Fix

--- change-digitalvibrance.sh
+++ change-digitalvibrance.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 #
 
-#little script for changing digital vibrance for nvidia GPU on linux
-# 
 
-# To create awesome greyscale like
-#./change-digitalvibrance.sh -1024
+change_settings(){
 
+    DPY=`nvidia-settings -q dpys | grep connected | awk -F " " '{print $3}' | sed 's/[(|)]//g'`
+    nvidia-settings -a :0.0/DigitalVibrance[${DPY}]=$1
 
-# To play csgo 
-#./change-digitalvibrance.sh 1023
-DPY=`nvidia-settings -q dpys | grep enabled | awk -F " " '{print $3}' | sed 's/[(|)]//g'`
-nvidia-settings -a :0.0/DigitalVibrance[${DPY}]=$1
+}
 
+case "$1" in
+        help)
+        echo "Usage: $0 [ < value between -1024 (nice greyscale) and 1023 (nice for counter strike > ]"
+        ;;
+        *)
+        change_settings $1
+RETVAL=$?
+esac
+exit 0

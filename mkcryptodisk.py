5a8baf37f3b61be7ed6be0774cadd06a82ea52a6
jmantas <jmantas@users.noreply.github.com>
2015-10-30 18:21:24 +0200

    argparse -> click

--- mkcryptodisk.py
+++ mkcryptodisk.py
@@ -3,35 +3,31 @@
 
 """ utility to make crypted disk files on *nix"""
 
-import argparse
+import click
 import os_env
 import block_device
 
-def main():
-    """ main function, gets parameters for creating crypted disk.
-        by default creates disk named <username>.dat.disk size 64MB 
-        with ext3 file system """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--disk", metavar='[disk name]', dest="disk_name",
-        help="disk name, defaults to username", type=str, default=str(os_env.get_disk_name()))
-    parser.add_argument("-s", "--size", metavar='[disk size]', dest="disk_size",
-        help="disk size in MB, defaults to 64MB", type=str, default="64")
-    parser.add_argument("-f", "--fstype", metavar='[file system type]', dest="fs_type",
-        help="file system type ( ext3, ext4, vfat, ... ), defaults to ext3", type=str, default="ext3")
-    parser.add_argument("-c", "--pgpencrypt", help="If set, also encrypt disk file with PGP", action="store_true")
-    args = parser.parse_args()
-
+@click.command()
+@click.option('--name', default="crypteddisk",
+                help='Disk name. Default = crypteddisk' )
+@click.option('--size', default=64,
+                help='Disk size. Default = 64M')
+@click.option('--fstype', default="ext3",
+                help='File system type. Default = ext3')
+@click.option('--pgp/--no-pgp', default=False, 
+                help='PGP encrypt. Default = False')
+def main(name, size, fstype, pgp):
     loop_device = str(os_env.get_loop_device())
-    mapper_interface = args.disk_name + ".crypt" 
+    mapper_interface = name + ".crypt" 
     mapper_interface_path = str(os_env.get_mapper_interface_full_path(mapper_interface))
-    file_system_type = args.fs_type
-    disk_size = args.disk_size
+    file_system_type = fstype
+    disk_size = size
 
-    file_disk_name = "%s/%s" % (os_env.get_user_homedir(), args.disk_name)
+    file_disk_name = "%s/%s" % (os_env.get_user_homedir(), name)
 
     """If disk file does not exist, create it"""
     if not os_env.file_checker(file_disk_name):
-        block_device.create_disk_file(file_disk_name, disk_size)
+        block_device.create_disk_file(file_disk_name, size)
     else:
         print "Exiting..."
         exit(1)
@@ -48,14 +44,15 @@ def main():
     """Creates file system on crypted mapper interface"""
     block_device.create_fs(file_system_type, mapper_interface_path)
    
-    """Checks if -c option is set, for additional PGP encryptio. If true - PGP crypts it""" 
-    if args.pgpencrypt:
+    """Checks if --pgp option is set, for additional PGP encryptio. If true - PGP crypts it""" 
+    if pgp:
         block_device.pgp_encrypt(file_disk_name)
         block_device.file_cleaner(file_disk_name)
 
     """Cleans not needed block devices"""
     block_device.remove_crypted_interface(mapper_interface)
     block_device.remove_loop_device(loop_device)
+
 
 if __name__ == "__main__":
     main()

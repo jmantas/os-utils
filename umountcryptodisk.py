5da473f8c039ce2a6fd1f48bb1aab0af9bdec312
jmantas <jmantas@users.noreply.github.com>
2015-11-03 18:33:09 +0200

    Added --pgp, --name option

--- umountcryptodisk.py
+++ umountcryptodisk.py
@@ -8,14 +8,17 @@ import os_env
 import block_device
 
 @click.command()
+@click.option('-n', '--name', default="crypteddisk",
+                help='Disk name. Default = crypteddisk' )
 @click.option('-m', "--mountpoint", help='Mountpoint')
 @click.option('-p', "--pgp/--no-pgp", default=False,
                 help='If set, also encrypt disk file with PGP')
-def main(mountpoint, pgp):
+def main(mountpoint, pgp, name):
     """ main function, gets parameters for unmount, and unmounts mountpoint defined by -m 
         and cleans up mapper interface and loop devices """
     
-    mount_point_to_unmount = mountpoint 
+    mount_point_to_unmount = mountpoint
+    file_disk_name = "%s/%s" % (os_env.get_user_homedir(), name)
     loop_device = str(os_env.get_loop_device())
     crypted_mapper_interface = str(os_env.get_mapper_interface())
     
@@ -24,14 +27,14 @@ def main(mountpoint, pgp):
     if block_device.check_mount_status(mount_point_to_unmount):
         block_device.unmount_mount_point(mount_point_to_unmount)
     
-    block_device.remove_crypted_interface(crypted_mapper_interface)
-    block_device.remove_loop_device(loop_device)
-    
-    """Checks if -p option is set, for additional PGP encryption. If true - PGP crypts it. 
+    """Checks if -p or --pgp option is set, for additional PGP encryption. If true - PGP crypts it. """ 
     if pgp:
         block_device.pgp_encrypt(file_disk_name)
         block_device.file_cleaner(file_disk_name)
-    """ 
+
+    block_device.remove_crypted_interface(crypted_mapper_interface)
+    block_device.remove_loop_device(loop_device)
+    
     
 if __name__ == "__main__":
     main()
